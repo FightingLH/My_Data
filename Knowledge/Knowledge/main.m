319a4b45aa1702a28ecb845f44ac3ee7c3e778b4
lihuan <976244150@qq.com>
2017-06-13 09:10:06 +0800

    资料总结

---
+++ Knowledge/Knowledge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Knowledge
+//
+//  Created by lh on 2017/6/13.
+//  Copyright © 2017年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
